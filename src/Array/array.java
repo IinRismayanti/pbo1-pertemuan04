b6d820331ee08048fcca6ee5b2b4b8319b067675
iinrismayanti <iiniinrismayanty@gmail.com>
2020-04-26 05:43:53 +0700

    Array

---
+++ src/Array/array.java
@@ -0,0 +1,15 @@
+package Array;
+
+public class array {
+    public static void main(String[] args) {
+       String mataKuliah[];
+       mataKuliah = new String [10];
+
+        mataKuliah[0] = "Pemprograman Berbasis Objek 1";
+        mataKuliah[0] = "Visual 2";
+        mataKuliah[0] = "Rekayasa Perangkat Lunak";
+        mataKuliah[0] = "Organisasi Komputer";
+        mataKuliah[0] = "Pemrograman Web 2";
+
+    }
+}
