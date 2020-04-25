3d3acd0f793e6c002f9e17c0979d596994df171e
iinrismayanti <iiniinrismayanty@gmail.com>
2020-04-26 05:29:47 +0700

    perulangan pada while

---
+++ src/Perulangan/loopwhile.java
@@ -0,0 +1,20 @@
+package Perulangan;
+
+public class loopwhile {
+    public static void main (String[] args){
+        int isiGelas = 0;
+        int penuh = 225;
+
+        System.out.println("IsI Gelas Sekarang :" + isiGelas + "ml");
+        System.out.println("IsI Gelas Ketika Penuh :" + penuh + "ml");
+
+        while (isiGelas != penuh){
+            isiGelas++;
+            System.out.println("Sedang mengisi gelas..");
+            System.out.println("Isi Gelas Sekarang :" + isiGelas +"ml");
+        }
+
+        System.out.println("Finale: Isi Gelas Sekarang : " + isiGelas + "ml");
+    }
+}
+
