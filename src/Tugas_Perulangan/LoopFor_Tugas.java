c5584d202b697ce23cf8c2a09cb35e00442f98ae
iinrismayanti <iiniinrismayanty@gmail.com>
2020-04-27 04:30:16 +0700

    Tugas Loop For

---
+++ src/Tugas_Perulangan/LoopFor_Tugas.java
@@ -0,0 +1,11 @@
+package Tugas_Perulangan;
+
+public class LoopFor_Tugas {
+        public static void main(String[] args) {
+                System.out.println ("Pengulangan Dengan For Arah Indeks Naik");
+                for (int i = 1; i <= 5; ++i) System.out.println ("Indeks Naik ke = "+i);
+                System.out.println ("Pengulangan Dengan For Arah Indeks Turun");
+                for (int i = 5; i > 0; --i)
+                        System.out.println ("Indeks Naik ke = "+i);
+        }
+}
